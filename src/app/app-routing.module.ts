33f608deec6100293553af29d938ce495b86a790
Graham Small <graham_small@live.co.uk>
2020-01-14 15:16:53 +0000

    Your First App Completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
+import { ProductDetailsComponent } from './product-details/product-details.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductListComponent } from './product-list/product-list';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ProductListComponent },
+  { path: 'products/:productId', component: ProductDetailsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
